84523aa1962738340df939a70569398de5206ade
DebabrataBanik <debabratabanik02@gmail.com>
2025-09-13 17:30:24 +0530

    add Home page
    mock error handling and render child components with api data

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,63 @@
+import InputField from "@/components/InputField";
+import { useGeolocation } from "../hooks/use-geolocation"
+import { useReverseGeocodeQuery, useWeatherQuery } from "../hooks/use-query";
+import CurrentWeatherData from "@/components/CurrentWeatherData";
+
+const Home = () => {
+
+  const { coordinates, error: locationError, isLoading: locationLoading, getLocation } = useGeolocation();
+  const weatherQuery = useWeatherQuery(coordinates);
+  const location = useReverseGeocodeQuery(coordinates);
+
+  console.log(location.data)
+  // console.log(coordinates)
+  // console.log(weatherQuery.data)
+
+  if (locationLoading) {
+    return <div>Someins wrong</div>
+  }
+
+  if (locationError) {
+    return (
+      <div>Enable Location</div>
+    )
+  }
+
+  if (!coordinates) {
+    return (
+      <div>Enable Location</div>
+    );
+  }
+
+  const locationData = location.data?.address;
+
+  if (weatherQuery.error) {
+    return (
+      <div>Weather Fetching error</div>
+    )
+  }
+
+  if (!weatherQuery.data) {
+    return <div>Someins wrong with weather data</div>
+  }
+
+  return (
+    <div className="flex flex-col mx-28">
+      <h1 className="font-bold text-[52px] text-center font-heading leading-[1.25] my-16">How’s the sky looking today?</h1>
+      <InputField />
+      <section className="mt-12">
+        <div className="flex gap-8">
+          <div className="flex flex-col w-[800px]">
+            <CurrentWeatherData data={weatherQuery.data} location={locationData} />
+            {/* Daily Forecast */}
+          </div>
+          <div className="w-1/3">
+            {/* Hourly Forecast */}
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Home
