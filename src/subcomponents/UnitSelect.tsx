63c2375b99f39303949a1b4e0a537312400ec9b6
DebabrataBanik <debabratabanik02@gmail.com>
2025-09-25 10:18:01 +0530

    feat: add units change logic
    use shadcn dropdown component

---
+++ src/subcomponents/UnitSelect.tsx
@@ -0,0 +1,66 @@
+import { useUnit } from "@/context/unit-context"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuLabel } from "@/components/ui/dropdown-menu"
+import UnitsIcon from '@/assets/images/icon-units.svg'
+import Dropdown from '@/assets/images/icon-dropdown.svg'
+import Check from '@/assets/images/icon-checkmark.svg'
+
+const UnitSelect = () => {
+
+  const { unit, setUnit } = useUnit()
+
+  // console.log('test')
+
+  const itemClass = (active: boolean) => `flex items-center justify-between text-base font-medium leading-[1.2] px-2 py-2.5 ${active ? 'bg-secondary text-white rounded-[8px]' : ''}`
+
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="rounded-[8px] px-4 py-3 bg-primary">
+          <button className="text-white flex gap-2.5 items-center cursor-pointer">
+            <img src={UnitsIcon} className="w-4 h-4" />
+            <span className="font-medium leading-2.5">Units</span>
+            <img src={Dropdown} className="w-3 h-5" />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="flex flex-col gap-1 w-[214px] bg-primary rounded-lg px-2 py-1.5 border">
+          {
+            unit === "metric" ?  
+            <DropdownMenuItem className="px-2 py-2.5 font-medium leading-[1.2] text-base hover:!bg-secondary hover:!text-white cursor-pointer" onClick={() => setUnit("imperial")}>
+              Switch to Imperial
+            </DropdownMenuItem>
+            :
+            <DropdownMenuItem className="px-2 py-2.5 font-medium leading-[1.2] text-base hover:!bg-secondary hover:!text-white cursor-pointer" onClick={() => setUnit("metric")}>
+              Switch to Metric
+            </DropdownMenuItem>
+          }
+
+          <DropdownMenuLabel className="text-sm font-medium text-muted-foreground leading-[1.2] px-2 pt-1.5">Temperature</DropdownMenuLabel>
+          <div>
+            <div className={itemClass(unit === 'metric')}>Celcius (°C)
+              {unit === "metric" && <span><img src={Check} /></span>}
+            </div>
+            <div className={itemClass(unit === 'imperial')}>Fahrenheit (°F) {unit === "imperial" && <span><img src={Check} /></span>}</div>
+          </div>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuLabel className="text-sm font-medium text-muted-foreground leading-[1.2] px-2 pt-1.5">Wind Speed</DropdownMenuLabel>
+          <div>
+            <div className={itemClass(unit === 'metric')}>km/h {unit === "metric" && <span><img src={Check} /></span>}</div>
+            <div className={itemClass(unit === 'imperial')}>mph {unit === "imperial" && <span><img src={Check} /></span>}</div>
+          </div>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuLabel className="text-sm font-medium text-muted-foreground leading-[1.2] px-2 pt-1.5">Precipitation</DropdownMenuLabel>
+          <div>
+            <div className={itemClass(unit === 'metric')}>Millimeters (mm) {unit === "metric" && <span><img src={Check} /></span>}</div>
+            <div className={itemClass(unit === 'imperial')}>Inches (in){unit === "imperial" && <span><img src={Check} /></span>}</div>
+          </div>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
+
+export default UnitSelect
