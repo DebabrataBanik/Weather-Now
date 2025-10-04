1fbf833d1eb4a6beb84727ebd69a6e276b659372
DebabrataBanik <debabratabanik02@gmail.com>
2025-10-04 16:19:30 +0530

    style: animate loading dots

--- src/components/skeleton/CurrentWeatherSkeleton.tsx
+++ src/components/skeleton/CurrentWeatherSkeleton.tsx
@@ -8,9 +8,9 @@ const CurrentWeatherSkeleton = () => {
     <div className="flex flex-col lg:gap-8 gap-5">
       <div className="flex flex-col items-center rounded-[20px] bg-primary justify-center h-[286px]">
         <div className="flex items-center gap-2.5 mb-3.5">
-          <span className="w-3 h-3 animate-pulse rounded-full bg-white/80"></span>
-          <span className="w-3 h-3 animate-pulse rounded-full -translate-y-1.5 bg-white/80"></span>
-          <span className="w-3 h-3 animate-pulse rounded-full bg-white/80"></span>
+          <span className="w-3 h-3 rounded-full animate-bouncy-dots delay-0 bg-white/80"></span>
+          <span className="w-3 h-3 rounded-full animate-bouncy-dots delay-100 -translate-y-1.5 bg-white/80"></span>
+          <span className="w-3 h-3 rounded-full animate-bouncy-dots delay-200 bg-white/80"></span>
         </div>
         <p className="font-medium text-lg leading-[1.2] text-accent-foreground">Loading...</p>
       </div>
