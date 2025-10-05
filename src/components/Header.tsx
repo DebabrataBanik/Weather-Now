d377ec09f4c4799eafe6ad268f566215bfe3fcaf
DebabrataBanik <debabratabanik02@gmail.com>
2025-10-06 03:14:19 +0530

    add alt tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
   return (
     <header className="mx-4 mt-4 sm:mx-6 sm:mt-6 xl:mx-28 lg:mt-12">
       <div className="flex items-center justify-between">
-        <img src={Logo} className="w-42 sm:w-52" />
+        <img src={Logo} alt='Logo' className="w-42 sm:w-52" />
         <div className="flex items-center gap-5">
           {/* <button 
             className={`flex items-center cursor-pointer transition-transform duration-500 ${isLight ? 'rotate-0' : 'rotate-180'}`}
